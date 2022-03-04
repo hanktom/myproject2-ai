76e1371476837ea78e973dcc86bdc5db4e8236c5
Hank Tom <hanktom@gmail.com>
2022-03-04 12:45:19 +0800

    Student class design

---
+++ src/main/java/com/tom/Scoring.java
@@ -0,0 +1,10 @@
+package com.tom;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student jack = new Student();
+        jack.name = "Jack";
+        jack.english = 75;
+        jack.math = 80;
+    }
+}
---
+++ src/main/java/com/tom/ScoringNG.java
@@ -0,0 +1,16 @@
+package com.tom;
+
+public class ScoringNG {
+
+    public static void main(String[] args) {
+        //5 students each english and math
+        int[] englishs = {75, 80, 88, 45, 78};
+        int[] maths = {80, 90, 100, 66, 77};
+        String[] name = {"Jack", "Hank", "Jane", "Elva", "Steve"};
+        for (int i = 0; i < 5; i++) {
+            if (i!=1) {
+                System.out.println(name[i] + "\t" + englishs[i] + "\t" + maths[i] + "\t" + (englishs[i] + maths[i]) / 2);
+            }
+        }
+    }
+}
