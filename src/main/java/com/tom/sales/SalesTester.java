19621944f59021f0bf74aab2ca22245fa98b6af4
Hank Tom <hanktom@gmail.com>
2022-04-01 12:40:54 +0800

    Polymorphism and Collection

--- src/main/java/com/tom/sales/SalesTester.java
+++ src/main/java/com/tom/sales/SalesTester.java
@@ -1,18 +1,27 @@
 package com.tom.sales;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SalesTester {
     public static void main(String[] args) {
-        Customer c1 = new Customer("001", 1200);
-        Customer c2 = new Customer("002", 800);
-        SilverCustomer c3 = new SilverCustomer("003", 2000);
-        Customer c4 = new Customer("006", 500);
-        GoldenCustomer c5 = new GoldenCustomer("007", 900);
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(800);
+        list.add(1200);
 
-        c1.print();
-        c2.print();
-        c3.print();
-        c4.print();
-        c5.print();
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("001", 1200));
+        customers.add(new Customer("002", 800));
+        customers.add(new SilverCustomer("003", 2000));
+        customers.add(new Customer("006", 500));
+        customers.add(new GoldenCustomer("007", 900));
+        for (int i = 0; i < 5; i++) {
+            customers.get(i).print();
+        }
+        for (Customer c : customers) {
+            c.print();
+        }
+
         //0001  1200    1100
     }
 }
